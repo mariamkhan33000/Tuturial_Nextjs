6d4a85c822657fef7d5cb0292ab2b867fd36b0b5
Mohsin Khan <mariamkhan33000@gmail.com>
2025-01-16 15:15:58 +0500

    Segment in Next Js

---
+++ src/app/blog/[...posts]/page.js
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+
+const Posts = ({ params }) => {
+  return (
+    <>
+    <h2>Blog: {params.posts[0]}</h2>
+    <h2>Post: {params.posts[1]}</h2>
+    </>
+  )
+}
+
+export default Posts
