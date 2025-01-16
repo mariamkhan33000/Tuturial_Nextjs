12772c695d0444f7db74e1de485344e6e4c87963
Mohsin Khan <mariamkhan33000@gmail.com>
2025-01-16 15:48:53 +0500

    Design 404 Page for all and Specific Folder Structure

---
+++ src/app/home/[...pageNotFound]/page.js
@@ -0,0 +1,13 @@
+// For route page error show in specific route page
+
+import React from 'react'
+
+const RouteBasePageNotFound = () => {
+  return (
+    <>
+    <h2>Page Not Found for Specific pagge | 404 Page</h2>
+    </>
+  )
+}
+
+export default RouteBasePageNotFound;
