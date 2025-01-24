b1f6b5e1c6871ea6ccfd6dedd39de3ba001b68cd
Mohsin Khan <mariamkhan33000@gmail.com>
2025-01-24 14:46:34 +0500

    Fetch Api Data in Server Side Components

---
+++ src/app/post/page.js
@@ -0,0 +1,20 @@
+async function getPosts() {
+    let postData = await fetch('https://jsonplaceholder.typicode.com/posts')
+    postData = await postData.json()
+    return postData;
+}
+
+const Post = async () => {
+    let posts = await getPosts();
+    // console.log(posts) 
+  return (
+    <div>
+        <h2>Posts | Fetch data with api in server side Components</h2>
+        {
+            posts.map((post) => <h4 key={post.id}>#{post.id}) Post Title :  {post.title}</h4>)
+        }
+    </div>
+  )
+}
+
+export default Post
